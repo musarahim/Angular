98ac7111d5985914a1ab585fc55196d48016ff57
Musa Rahim <musarahim13@gmail.com>
2021-11-16 08:17:35 +0300

    retrieved data using http

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from "@angular/core"
+import {Component, OnDestroy, OnInit} from "@angular/core"
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
 
@@ -8,11 +9,13 @@ import { ProductService } from "./product.service";
     styleUrls:['./product-list.component.css']
 
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit, OnDestroy{
     pageTitle:string="Product List";
     imageWidth:number=50;
     imageMargin:number=2;
     showImage:boolean=false;
+    errorMessage:string ='';
+    sub!: Subscription;
     private _listFilter:string='';
     get listFilter():string{
       return this._listFilter;
@@ -37,11 +40,21 @@ export class ProductListComponent implements OnInit{
           this.showImage= !this.showImage;
         }
         ngOnInit():void{
-        this.products = this.productService.getProducts();
-        this.filteredProducts= this.products
+        this.sub= this.productService.getProducts().subscribe({
+          next:products => {
+            this.products = products;
+            this.filteredProducts= this.products;
+          },
+          error:err => this.errorMessage = err,
+
+        });
+        
         }
         onRatingClicked(message:string):void{
           this.pageTitle='Product List '+ message;
 
+        }
+        ngOnDestroy(){
+         this.sub.unsubscribe();
         }
 }
