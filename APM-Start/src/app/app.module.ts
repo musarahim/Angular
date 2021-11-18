f750393a8a1cec62e980e3cdb35a2aea0b8f7f32
Musa Rahim <musarahim13@gmail.com>
2021-11-18 11:38:24 +0300

    added the product and shared modules

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,28 +1,28 @@
  import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms'
 import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
+
 import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
+    RouterModule.forRoot([
+    
+      {path:'welcome', component:WelcomeComponent},
+      {path:'', redirectTo:'welcome',pathMatch:'full'},
+      {path:'**', redirectTo:'welcome', pathMatch:'full'}
+    ]),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
